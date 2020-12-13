8e4717675e5db0a161e97830c02a926c2020d8eb
tuan.tn1 <ngoctuan.ktmm@gmail.com>
2020-12-13 21:24:59 +0700

    Done Flutter-10

--- Flutter_10/flutter_10/lib/myapp.dart
+++ Flutter_10/flutter_10/lib/myapp.dart
@@ -8,26 +8,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  final _contentController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  // define states
+  String _content;
+  double _amount;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "This is a StatefulWidget",
       home: Scaffold(
+        key: _scaffoldKey,
         body: SafeArea(
+          minimum: const EdgeInsets.only(left: 20, right: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               TextField(
                 decoration: InputDecoration(labelText: 'Content'),
+                controller: _contentController,
+                onChanged: (text) {
+                  setState(() {
+                    _content = text;
+                  });
+                },
               ),
               TextField(
-                decoration: InputDecoration(labelText: 'Amount(money'),
+                decoration: InputDecoration(labelText: 'Amount(money)'),
+                controller: _amountController,
+                onChanged: (text) {
+                  setState(() {
+                    _amount = double.tryParse(text) ?? 0;
+                  });
+                },
               ),
               FlatButton(
                 child: Text('Insert Transaction'),
-                textColor: Colors.blue,
+                color: Colors.blue,
+                textColor: Colors.white,
                 onPressed: () {
-
+                  print('Content = $_content, money\'s amount = $_amount');
+                  // Display to UI
+                  _scaffoldKey.currentState.showSnackBar(SnackBar(
+                    content:
+                        Text('Content = $_content, money\'s amount = $_amount'),
+                    duration: Duration(seconds: 3),
+                  ));
                 },
               )
             ],
@@ -36,5 +66,4 @@ class _MyApp extends State<MyApp> {
       ),
     );
   }
-
 }
